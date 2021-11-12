8856a18719750539b9ecd7a0139bdac61d2288bb
Elichai Turkel <elichai.turkel@gmail.com>
2021-11-12 18:55:49 +0200

    Optimize the hasher creations

--- src/pow/hasher.rs
+++ src/pow/hasher.rs
@@ -1,18 +1,23 @@
 use crate::Hash;
+use once_cell::sync::Lazy;
 use sha3::digest::{ExtendableOutput, Update, XofReader};
 use sha3::CShake256;
 
 const PROOF_OF_WORK_DOMAIN: &[u8] = b"ProofOfWorkHash";
 const HEAVY_HASH_DOMAIN: &[u8] = b"HeavyHash";
 
+#[derive(Clone)]
 pub(super) struct PowHasher(CShake256);
 
+#[derive(Clone)]
 pub(super) struct HeavyHasher(CShake256);
 
 impl PowHasher {
     #[inline]
     pub(super) fn new() -> Self {
-        Self(CShake256::new(PROOF_OF_WORK_DOMAIN))
+        static POW_HASHER: Lazy<PowHasher> =
+            Lazy::new(|| Self(CShake256::new(PROOF_OF_WORK_DOMAIN)));
+        (*POW_HASHER).clone()
     }
 
     pub(super) fn write<A: AsRef<[u8]>>(&mut self, data: A) {
@@ -30,7 +35,9 @@ impl PowHasher {
 impl HeavyHasher {
     #[inline]
     pub(super) fn new() -> Self {
-        Self(CShake256::new(HEAVY_HASH_DOMAIN))
+        static HEAVY_HASHER: Lazy<HeavyHasher> =
+            Lazy::new(|| Self(CShake256::new(HEAVY_HASH_DOMAIN)));
+        (*HEAVY_HASHER).clone()
     }
 
     pub(super) fn write<A: AsRef<[u8]>>(&mut self, data: A) {
