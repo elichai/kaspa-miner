a4f0323605b09142b969394682103a39942fb5e8
Elichai Turkel <elichai.turkel@gmail.com>
2021-11-13 16:03:18 +0200

    Refactor kaspad handler

--- src/client.rs
+++ src/client.rs
@@ -6,87 +6,18 @@ use crate::Error;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::error::SendError;
 use tokio::sync::mpsc::Sender;
-use tokio::task;
-use tokio::task::JoinHandle;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::transport::Channel as TonicChannel;
 use tonic::Streaming;
 
-pub struct GrpcClient {
-    client: RpcClient<TonicChannel>,
-    listen_handler: JoinHandle<Result<KaspadHandler, Error>>,
-    send_channel: Sender<KaspadMessage>,
-}
-
 pub struct KaspadHandler {
+    client: RpcClient<TonicChannel>,
     send_channel: Sender<KaspadMessage>,
     stream: Streaming<KaspadMessage>,
     miner_address: String,
 }
 
 impl KaspadHandler {
-    pub fn listen(
-        stream: Streaming<KaspadMessage>,
-        send_channel: Sender<KaspadMessage>,
-        miner_address: String,
-    ) -> JoinHandle<Result<KaspadHandler, Error>> {
-        task::spawn(async move {
-            let mut miner = MinerManager::new(send_channel.clone());
-            let mut handler = Self {
-                send_channel,
-                stream,
-                miner_address,
-            };
-            while let Some(msg) = handler.stream.message().await? {
-                match msg.payload {
-                    Some(payload) => match payload {
-                        Payload::BlockAddedNotification(_) => {
-                            let get_block_template = GetBlockTemplateRequestMessage {
-                                pay_address: handler.miner_address.clone(),
-                            };
-                            handler.send_channel.send(get_block_template.into()).await?;
-                        }
-                        Payload::GetBlockTemplateResponse(template) => {
-                            if let Some(err) = template.error {
-                                println!("Error! {:?}", err);
-                                continue;
-                            }
-                            match template.block {
-                                Some(b) => {
-                                    if let Err(e) = miner.process_block(b).await {
-                                        println!("Failed processing block: {}", e);
-                                    }
-                                }
-                                None => {
-                                    println!("No block and No Error!");
-                                    continue;
-                                }
-                            }
-                        }
-                        msg => println!("got unknown msg: {:?}", msg),
-                    },
-                    None => println!("payload is empty"),
-                }
-            }
-            Ok(handler)
-        })
-    }
-
-    // async fn handle_msg(&self, msg: Payload) -> Result<(), Error> {
-    //     match msg {
-    //         Payload::BlockAddedNotification(notf) => {
-    //             let get_block_template = GetBlockTemplateRequestMessage{ pay_address: self.miner_address.clone() };
-    //             let send = self.client_send(get_block_template);
-    //             let send = send.await;
-    //             send?;
-    //         }
-    //         _ => println!("got unknown msg: {:?}", msg),
-    //     }
-    //     Ok(())
-    // }
-}
-
-impl GrpcClient {
     pub async fn connect<D>(address: D, miner_address: String) -> Result<Self, Error>
     where
         D: std::convert::TryInto<tonic::transport::Endpoint>,
@@ -99,11 +30,11 @@ impl GrpcClient {
             .message_stream(ReceiverStream::new(recv))
             .await?
             .into_inner();
-        let listen_handler = KaspadHandler::listen(stream, send_channel.clone(), miner_address);
         Ok(Self {
             client,
-            listen_handler,
+            stream,
             send_channel,
+            miner_address,
         })
     }
 
@@ -112,5 +43,43 @@ impl GrpcClient {
         msg: impl Into<KaspadMessage>,
     ) -> Result<(), SendError<KaspadMessage>> {
         self.send_channel.send(msg.into()).await
+    }
+
+    pub async fn listen(&mut self) -> Result<(), Error> {
+        let mut miner = MinerManager::new(self.send_channel.clone());
+        while let Some(msg) = self.stream.message().await? {
+            match msg.payload {
+                Some(payload) => self.handle_message(payload, &mut miner).await?,
+                None => println!("payload is empty"),
+            }
+        }
+        Ok(())
+    }
+
+    async fn handle_message(&self, msg: Payload, miner: &mut MinerManager) -> Result<(), Error> {
+        match msg {
+            Payload::BlockAddedNotification(_) => {
+                let get_block_template = GetBlockTemplateRequestMessage {
+                    pay_address: self.miner_address.clone(),
+                };
+                self.send_channel.send(get_block_template.into()).await?;
+            }
+            Payload::GetBlockTemplateResponse(template) => match (template.block, template.error) {
+                (Some(b), None) => {
+                    if let Err(e) = miner.process_block(b).await {
+                        println!("Failed processing block: {}", e);
+                    }
+                }
+                (None, Some(e)) => println!("GetTemplate returned with an error: {:?}", e),
+                (Some(_), Some(e)) => println!("GetTemplate returned with block&error: {:?}", e),
+                (None, None) => println!("No block and No Error!"),
+            },
+            Payload::SubmitBlockResponse(res) => match res.error {
+                None => println!("Submitted block successfully!"),
+                Some(e) => println!("Failed submitting block: {:?}", e),
+            },
+            msg => println!("got unknown msg: {:?}", msg),
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub mod proto {
     // tonic::include_proto!("protowire"); // FIXME: https://github.com/intellij-rust/intellij-rust/issues/6579
     include!("protowire.rs");
 }
-use crate::client::GrpcClient;
+use crate::client::KaspadHandler;
 use crate::proto::NotifyBlockAddedRequestMessage;
 use std::error::Error as StdError;
 
@@ -18,13 +18,13 @@ pub type Error = Box<dyn StdError + Send + Sync + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client = GrpcClient::connect(
-        "grpc://localhost:16110",
-        "kaspa:qq9frgnvaa3zg9qhh9t8s6vkf6x7rxqlnemgcj72y9enf2hsujxd276dtukjm".to_string(),
-    )
-    .await?;
+    let miner_address =
+        "kaspa:qq9frgnvaa3zg9qhh9t8s6vkf6x7rxqlnemgcj72y9enf2hsujxd276dtukjm".to_string();
+    let mut client = KaspadHandler::connect("grpc://localhost:16110", miner_address).await?;
+
     client
         .client_send(NotifyBlockAddedRequestMessage {})
         .await?;
-    loop {}
+
+    client.listen().await
 }
