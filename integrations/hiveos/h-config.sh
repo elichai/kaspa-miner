4a9342704b86caa4bda585eb3370c21a75d0c093
Tomer Levy <tmrlvi@gmail.com>
2022-01-16 12:13:52 +0200

    Added Merlin's files

---
+++ integrations/hiveos/h-config.sh
@@ -0,0 +1,21 @@
+####################################################################################
+###
+### kaspa-miner
+### https://github.com/tmrlvi/kaspa-miner/releases
+###
+### Hive integration: Merlin
+###
+####################################################################################
+
+#!/usr/bin/env bash
+[[ -e /hive/custom ]] && . /hive/custom/kaspa-miner/h-manifest.conf
+[[ -e /hive/miners/custom ]] && . /hive/miners/custom/kaspa-miner/h-manifest.conf
+conf=""
+conf+=" --kaspad-address=$CUSTOM_URL --mining-address $CUSTOM_TEMPLATE"
+
+
+[[ ! -z $CUSTOM_USER_CONFIG ]] && conf+=" $CUSTOM_USER_CONFIG"
+
+echo "$conf"
+echo "$conf" > $CUSTOM_CONFIG_FILENAME
+
