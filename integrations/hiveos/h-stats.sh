b702c471091e291d647b50e0af02795c3c55a12e
Ben R <mozaiti@gmail.com>
2022-01-18 15:59:47 +0800

    add gpu status display, gpu hashrate is just an average figure

--- integrations/hiveos/h-stats.sh
+++ integrations/hiveos/h-stats.sh
@@ -27,14 +27,57 @@ diffTime=`echo $((time_now-time_rep)) | tr -d '-'`
 
 if [ "$diffTime" -lt "$maxDelay" ]; then
         total_hashrate=`echo $stats_raw | awk '{print $7}' | cut -d "." -f 1,2 --output-delimiter='' | sed 's/$/0/'`
-        if [[ $stats_raw == *"Ghash"* ]]; then
-                total_hashrate=$(($total_hashrate*1000))
-        fi
+	if [[ $stats_raw == *"Ghash"* ]]; then
+		total_hashrate=$(($total_hashrate*1000))
+	fi
+
+        #GPU Status
+        gpu_stats=$(< $GPU_STATS_JSON)
+
+        readarray -t gpu_stats < <( jq --slurp -r -c '.[] | .busids, .brand, .temp, .fan | join(" ")' $GPU_STATS_JSON 2>/dev/null)
+        busids=(${gpu_stats[0]})
+        brands=(${gpu_stats[1]})
+        temps=(${gpu_stats[2]})
+        fans=(${gpu_stats[3]})
+        count=${#busids[@]}
+
+        hash_arr=()
+        busid_arr=()
+        fan_arr=()
+        temp_arr=()
+        lines=()
+
+        avg_hash_per_gpu=$((total_hashrate/$count))
+
+        idx=0
+        for(( i=0; i < count; i++ )); do
+                [[ "${busids[i]}" =~ ^([A-Fa-f0-9]+): ]]
+                busid_arr+=($((16#${BASH_REMATCH[1]})))
+                temp_arr+=(${temps[i]})
+                fan_arr+=(${fans[i]})
+                # TODO, get hash per gpu
+                hash_arr+=($avg_hash_per_gpu)
+                ((idx++))
+        done
+
+        hash_json=`printf '%s\n' "${hash_arr[@]}" | jq -cs '.'`
+        bus_numbers=`printf '%s\n' "${busid_arr[@]}"  | jq -cs '.'`
+        fan_json=`printf '%s\n' "${fan_arr[@]}"  | jq -cs '.'`
+        temp_json=`printf '%s\n' "${temp_arr[@]}"  | jq -cs '.'`
+
+        uptime=$(( `date +%s` - `stat -c %Y $CUSTOM_CONFIG_FILENAME` ))
+
+
+        #Compile stats/khs
         stats=$(jq -nc \
-                --argjson hs "[$total_hashrate]"\
+                --argjson hs "$hash_json"\
                 --arg ver "$CUSTOM_VERSION" \
                 --arg ths "$total_hashrate" \
-                '{ hs: $hs, hs_units: "khs", algo : "heavyhash", ver:$ver }')
+                --argjson bus_numbers "$bus_numbers" \
+                --argjson fan "$fan_json" \
+                --argjson temp "$temp_json" \
+                --arg uptime "$uptime" \
+                '{ hs: $hs, hs_units: "khs", algo : "heavyhash", ver:$ver , $uptime, $bus_numbers, $temp, $fan}')
         khs=$total_hashrate
 else
   khs=0
