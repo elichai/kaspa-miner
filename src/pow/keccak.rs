8c237ed57435b5a12826f720f82e9fcc24c88ce7
Elichai Turkel <elichai.turkel@gmail.com>
2023-08-05 15:44:50 +0300

    Update asm and add windows asm

--- src/pow/keccak.rs
+++ src/pow/keccak.rs
@@ -1,9 +1,9 @@
-#[cfg(any(not(target_arch = "x86_64"), feature = "no-asm", target_os = "windows"))]
+#[cfg(any(not(target_arch = "x86_64"), feature = "no-asm"))]
 pub(super) fn f1600(state: &mut [u64; 25]) {
     keccak::f1600(state);
 }
 
-#[cfg(all(target_arch = "x86_64", not(feature = "no-asm"), not(target_os = "windows")))]
+#[cfg(all(target_arch = "x86_64", not(feature = "no-asm")))]
 pub(super) fn f1600(state: &mut [u64; 25]) {
     extern "C" {
         fn KeccakF1600(state: &mut [u64; 25]);
